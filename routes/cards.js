07a935366e873221a49ca4b3e72101c0998d80ac
Alex Spark <san4es.komgmail.com>
2023-05-24 15:49:03 +0300

    fix Обработка неправильного пути

--- routes/cards.js
+++ routes/cards.js
@@ -8,8 +8,6 @@ router.get('/', getCards);
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', putCardLike);
 router.delete('/:cardId/likes', deleteCardLike);
-router.use((req, res) => {
-  res.status(404).send({ message: 'Запрашиваемая страница не найдена' });
-});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,6 @@ router.get('/:_id', getUserId);
 router.post('/', createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateUserAvatar);
-router.use((req, res) => {
-  res.status(404).send({ message: 'Запрашиваемая страница не найдена' });
-});
+
 
 module.exports = router;
