890d7e065468fcd39361af8369742fe85366ba61
Alex Spark <san4es.komgmail.com>
2023-05-26 14:29:39 +0300

    fix: Логика контроллеров исправлена и вынесена в общий компонент

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,112 +1,40 @@
 const mongoose = require('mongoose');
 const Card = require('../models/card');
-const User = require('../models/user');
+const wrapper = require('./wrapper');
 const {
-  SUCCESS, CREATE, BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR
+  CREATE
 } = require('../constants/ErrorStatuses');
 
-const getCards = (req, res) => {
-  Card.find({})
-    .then((cards) => {
-      res.status(SUCCESS).send(cards);
-    })
-    .catch((err) => {
-      res.status(INTERNAL_SERVER_ERROR).send({
-        message: 'Internal Server Error',
-        stack: err.stack
-      });
-    });
-};
+const getCards = wrapper((req, res) => {
+  return Card.find({})
+});
 
-const createCards = async (req, res) => {
-  try {
-    const card = await Card.create({
-      owner: req.user._id,
-      ...req.body
-    });
-    res.status(CREATE).send(card);
-  } catch (err) {
-    if (err instanceof mongoose.Error.ValidationError) {
-      res.status(BAD_REQUEST).send({
-        message: 'Incorrect data sent'
-      });
-      return;
-    }
-    res.status(INTERNAL_SERVER_ERROR).send({
-      message: 'Internal Server Error',
-      stack: err.stack
-    });
-  }
-};
+const createCards = wrapper((req, res) => {
+  return Card.create({
+    owner: req.user._id,
+    ...req.body
+  })
+}, CREATE)
 
-const deleteCard = (req, res) => {
-  Card.findByIdAndDelete(req.params.cardId)
-    .then((card) => {
-      if (!card) {
-        res.status(NOT_FOUND).send({ message: 'Card Id not found' });
-        return;
-      }
-      res.status(SUCCESS).send(card);
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.CastError) {
-        res.status(BAD_REQUEST).send({ message: 'Invalid Card Id' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR).send({
-        message: 'Internal Server Error',
-        stack: err.stack
-      });
-    });
-};
+const deleteCard = wrapper((req, res) => {
+  return Card.findByIdAndDelete(req.params.cardId)
+})
 
-const putCardLike = async (req, res) => {
-  try {
-    const cardLike = await Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true }
-    );
-    if (!cardLike) {
-      res.status(NOT_FOUND).send({ message: 'Card Not Found' });
-      return;
-    }
-    res.status(SUCCESS).send(cardLike);
-  } catch (err) {
-    if (err instanceof mongoose.Error.CastError) {
-      res.status(BAD_REQUEST).send({ message: 'Invalid Card Id' });
-      return;
-    }
-    res.status(INTERNAL_SERVER_ERROR).send({
-      message: 'Internal Server Error',
-      stack: err.stack
-    });
-  }
-};
+const putCardLike = wrapper((req, res) => {
+  return Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true, runValidators: true }
+  )
+})
 
-const deleteCardLike = async (req, res) => {
-  try {
-    const cardLike = await Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } },
-      { new: true }
-    );
-    if (!cardLike) {
-      res.status(NOT_FOUND).send({ message: 'Card Not Found' });
-      return;
-    }
-    res.status(SUCCESS).send(cardLike);
-  } catch (err) {
-    if (err instanceof mongoose.Error.CastError) {
-      res.status(BAD_REQUEST).send({ message: 'Invalid Card Id' });
-      return;
-    }
-    res.status(INTERNAL_SERVER_ERROR).send({
-      message: 'Internal Server Error',
-      stack: err.stack
-    });
-  }
-};
+const deleteCardLike = wrapper((req, res) => {
+  return Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true, runValidators: true }
+  )
+})
 
 module.exports = {
   createCards,
--- controllers/users.js
+++ controllers/users.js
@@ -1,156 +1,38 @@
 const mongoose = require('mongoose')
 const User = require('../models/user');
 const {
-  SUCCESS, CREATE, BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR
+  CREATE
 } = require('../constants/ErrorStatuses');
 
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.status(SUCCESS).send(users);
-    })
-    .catch((err) => {
-      res.status(INTERNAL_SERVER_ERROR).send({
-        message: 'Internal Server Error',
-        stack: err.stack
-      });
-    });
-};
+const wrapper = require('./wrapper');
 
-const createUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(CREATE).send(user);
-  } catch (err) {
-    if (err instanceof mongoose.Error.ValidationError) {
-      res.status(BAD_REQUEST).send({
-        message: 'Incorrect data sent'
-      });
-      return;
-    }
-    res.status(INTERNAL_SERVER_ERROR).send({
-      message: 'Internal Server Error',
-      stack: err.stack
-    });
-  }
-};
+const getUsers = wrapper((req, res) => {
+  return User.find({});
+});
 
-const getUserId = (req, res) => {
-  User.findById(req.params._id)
-    .then((user) => {
-      if (!user) {
-        res.status(NOT_FOUND).send({ message: 'User Not Found' });
-        return;
-      }
-      res.status(SUCCESS).send(user);
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.CastError) {
-        res.status(BAD_REQUEST).send({ message: 'Invalid User Id' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR).send({
-        message: 'Internal Server Error',
-        stack: err.stack
-      });
-    });
-};
+const createUser = wrapper((req, res) => {
+  return User.create(req.body);
+}, CREATE)
 
-// const updateUser = async (req, res) => {
-//   try {
-//     const user = await User.findByIdAndUpdate(
-//       req.user._id,
-//       { name: req.body.name, about: req.body.about },
-//       { new: true, runValidators: true }
-//     )
-//     if (!user) {
-//       res.status(NOT_FOUND).send({ message: 'User Not Found' });
-//       return;
-//     }
-//     res.status(SUCCESS).send(user);
-//   } catch (err) {
-//     if (err instanceof mongoose.Error.ValidationError) {
-//       res.status(BAD_REQUEST).send({
-//         message: 'Incorrect data sent'
-//       });
-//       return;
-//     }
-//     if (err instanceof mongoose.Error.CastError) {
-//       res.status(BAD_REQUEST).send({ message: 'Invalid User Id' });
-//       return;
-//     }
-//     res.status(INTERNAL_SERVER_ERROR).send({
-//       message: 'Internal Server Error',
-//       stack: err.stack
-//     });
-//   }
-// };
+const getUserId = wrapper((req, res) => {
+  return User.findById(req.params._id);
+})
 
-const updateUser = (req, res) => {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(
+const updateUser = ((req, res) => {
+  return User.findByIdAndUpdate(
     req.user._id,
-    { name, about },
+    { name: req.body.name, about: req.body.about },
+    { new: true, runValidators: true }
+  );
+})
+
+const updateUserAvatar = wrapper((req, res) => {
+  return User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: req.user.avatar },
     { new: true, runValidators: true }
   )
-  .then((user) => {
-    console.log(user)
-    if (!user) {
-      res.status(NOT_FOUND).send({ message: 'User Not Found' });
-      return;
-    }
-    res.status(SUCCESS).send(user);
-  })
-  .catch((err) => {
-    console.log(err)
-    if (err instanceof mongoose.Error.CastError) {
-      res.status(BAD_REQUEST).send({ message: 'Invalid User Id' });
-      return;
-    }
-    if (err instanceof mongoose.Error.ValidationError) {
-      res.status(BAD_REQUEST).send({
-        message: 'Incorrect data sent'
-      });
-      return;
-    }
-    res.status(INTERNAL_SERVER_ERROR).send({
-      message: 'Internal Server Error',
-      stack: err.stack
-    });
-  });
-};
-
-const updateUserAvatar = async (req, res) => {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    { new: true, runValidators: true }
-  )
-    .then((user) => {
-      if (!user) {
-        res.status(NOT_FOUND).send({ message: 'User Not Found' });
-        return;
-      }
-      res.status(SUCCESS).send(user);
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.CastError) {
-        res.status(BAD_REQUEST).send({ message: 'Invalid User Id' });
-        return
-      }
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(BAD_REQUEST).send({
-          message: 'Incorrect data sent'
-        });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR).send({
-        message: 'Internal Server Error',
-        stack: err.stack
-      });
-    });
-};
+})
 
 module.exports = {
   getUsers,
